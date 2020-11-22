ed46ea2584f7b03bd13c30ab1f6b51d703b6b0d2
Alistair Evans <alistairjevans@gmail.com>
2020-11-22 09:14:39 +0000

    Updates

---
+++ precache-manifest.a97eb350f638b935fa3eb685db268c2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae46c92ddd07dd5b00aecad5b034af02",
+    "url": "/collage-editor/index.html"
+  },
+  {
+    "revision": "b39fe27b2cae64300dee",
+    "url": "/collage-editor/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "daf40ed17459c2e14d7c",
+    "url": "/collage-editor/static/js/2.362fc0e0.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/collage-editor/static/js/2.362fc0e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b39fe27b2cae64300dee",
+    "url": "/collage-editor/static/js/main.5f9f81da.chunk.js"
+  },
+  {
+    "revision": "d8d38bbf3831630710be",
+    "url": "/collage-editor/static/js/runtime-main.25878b95.js"
+  }
+]);
