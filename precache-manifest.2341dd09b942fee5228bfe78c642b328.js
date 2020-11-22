c435c858ae99822fe192c04421dae3e2d0463105
Alistair Evans <alistairjevans@gmail.com>
2020-11-22 09:57:56 +0000

    Updates

---
+++ precache-manifest.2341dd09b942fee5228bfe78c642b328.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f0c0e9bd413da97fc16b97a0346ea86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "298b4dcd06d2f03b2105",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "daf40ed17459c2e14d7c",
+    "url": "/static/js/2.362fc0e0.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.362fc0e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "298b4dcd06d2f03b2105",
+    "url": "/static/js/main.660c2a52.chunk.js"
+  },
+  {
+    "revision": "524905a406a0a0a20a4b",
+    "url": "/static/js/runtime-main.d5bdc60f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/collage-editor/precache-manifest.e250913302fc003ae994675c0dd08b8c.js"
+  "/precache-manifest.2341dd09b942fee5228bfe78c642b328.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/collage-editor/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
